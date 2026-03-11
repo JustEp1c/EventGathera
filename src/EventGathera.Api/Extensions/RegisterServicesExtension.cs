bfd7fefc4a03b98a983b01a3ff2df1e48782f1b2
JustEp1c <timofey_vdv@mail.ru>
2026-03-12 02:28:54 +0300

    Регистрация сервисов в DI-контейнере

---
+++ src/EventGathera.Api/Extensions/RegisterServicesExtension.cs
@@ -0,0 +1,23 @@
+﻿using EventGathera.Api.Services.Implementations;
+using EventGathera.Api.Services.Interfaces;
+
+namespace EventGathera.Api.Extensions;
+
+
+/// <summary>
+/// Статический класс расширения для регистрации сервисов
+/// </summary>
+public static class RegisterServicesExtension
+{
+    /// <summary>
+    /// Зарегистрировать сервисы
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEventService, EventService>();
+
+        return services;
+    }
+}
--- src/EventGathera.Api/Program.cs
+++ src/EventGathera.Api/Program.cs
@@ -1,8 +1,12 @@
+using EventGathera.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterServices();
 
 var app = builder.Build();
 
