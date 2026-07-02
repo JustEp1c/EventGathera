90be16a99f63bdb9be60946045b20f48800718cf
JustEp1c <timofey_vdv@mail.ru>
2026-07-02 18:10:39 +0300

    Создание и применение миграций

--- src/EventGathera.Api/Program.cs
+++ src/EventGathera.Api/Program.cs
@@ -1,6 +1,7 @@
 using EventGathera.Api.DataAccess;
 using EventGathera.Api.Extensions;
 using EventGathera.Api.Extensions.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,7 +23,13 @@ if (app.Environment.IsDevelopment())
         options.ValidateOnBuild = true;
     });
 
-}   
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
