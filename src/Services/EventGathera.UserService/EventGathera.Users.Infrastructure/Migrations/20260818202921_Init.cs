8647a2f5e743a0182724a3ab6473315d960c2be5
JustEp1c <timofey_vdv@mail.ru>
2026-08-19 00:07:03 +0300

    Созданы миграции для сервисов

---
+++ src/Services/EventGathera.UserService/EventGathera.Users.Infrastructure/Migrations/20260818202921_Init.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventGathera.Users.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    login = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_login",
+                table: "users",
+                column: "login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
