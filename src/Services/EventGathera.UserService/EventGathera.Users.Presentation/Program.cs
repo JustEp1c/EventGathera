f0adb239e1b3e5bc6601269aaaf999bf882f6432
JustEp1c <timofey_vdv@mail.ru>
2026-08-27 01:56:58 +0300

    Добавлено Serilog логирование и Grafana, добавлен файл с дашбордом

--- src/Services/EventGathera.UserService/EventGathera.Users.Presentation/Program.cs
+++ src/Services/EventGathera.UserService/EventGathera.Users.Presentation/Program.cs
@@ -2,10 +2,16 @@ using EventGathera.Presentation.Extensions;
 using EventGathera.Presentation.Extensions.Middleware;
 using EventGathera.Users.Infrastructure.DataAccess;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Formatting.Compact;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.RegisterServices(builder.Configuration);
+
+builder.Host.UseSerilog((ctx, cfg) =>
+    cfg.ReadFrom.Configuration(ctx.Configuration)
+       .WriteTo.Console(new CompactJsonFormatter()));
 
 var app = builder.Build();
 
